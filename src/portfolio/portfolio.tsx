9b749aad0674a720e1cddddfb289d335edc00997
Gustavo Gonzaga <gustavosg91@gmail.com>
2024-02-28 22:43:32 -0300

    Feat: portfolio session

--- src/portfolio/portfolio.tsx
+++ src/portfolio/portfolio.tsx
@@ -1,13 +1,21 @@
 import './portfolio.css'
+import projectOne from '../assets/PRINT1.png';
+import projectTwo from '../assets/PRINT2.png';
+import projectThree from '../assets/PRINT3.png';
 
 const portfolio = () => {
 
- const titlePortfolio = "<MY LAST PROJECTS/>"
+ const titlePortfolio = "<MY LASTEST PROJECTS/>"
 
   return (
     <div>
         <div className="port-container">
             <p className='titlePort'>{titlePortfolio}</p>
+            <div className="imagesProjects">
+              <img className='image-project' src={projectOne} alt="printprojeto1" />
+              <img className='image-project' src={projectTwo} alt="printprojeto1" />
+              <img className='image-project' src={projectThree} alt="printprojeto1" />
+            </div>
         </div>
     </div>
   )
