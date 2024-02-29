c22150184682f9a836d351a59d6d81731e1c50a6
Gustavo Gonzaga <gustavosg91@gmail.com>
2024-02-28 23:54:20 -0300

    Feat: responsive at 1024px

--- src/header/header.tsx
+++ src/header/header.tsx
@@ -3,14 +3,17 @@ import logo from '../assets/LOGO-EU-DEV.png'
 
 const header = () => {
     return (
-        <div className='header'>
-            <img className='Logo' src={logo} alt="logo" />
-            <nav className='nav'>
-                <a href="/">Home</a>
-                <a href="/">Portfolio</a>
-                <a href="/">Resume</a>
-            </nav>
-        </div >
+        <div className='topo'>
+            <div className='header'>
+                <img className='Logo' src={logo} alt="logo" />
+                <nav className='nav'>
+                    <a href="/">Home</a>
+                    <a href="/">Portfolio</a>
+                    <a href="/">Resume</a>
+                </nav>
+            </div>
+        </div>
+
     )
 }
 
